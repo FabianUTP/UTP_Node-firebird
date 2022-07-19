af18dd714bfe0ab320f290f2a9bb97c0836701f3
FabianCaamalHaas <fabian@ecohete.com>
2022-07-19 09:42:28 -0600

    doc en index

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ require('dotenv');
 // Plantillas Handlebars
 app.set('view engine', 'hbs');
 
+// Aqui se definen las rutas
 app.use(require('./routes/routes'));
 
+
+// Aqui se levanta el servidor y se define el puerto
 app.listen(3000, () => {
     console.log('escuchando en el puerto 3000');
 })

41045d76c29042205dc9aa996679618de2230cf6
FabianUTP <a.fabian.caamal@utponiente.edu.mx>
2023-03-13 15:53:56 -0600

    se agregaron botones y se modificaron algunas cosas

--- public/js/services/admin/setSubirCalificacion.js
+++ public/js/services/admin/setSubirCalificacion.js
@@ -1,6 +1,8 @@
 const table = document.getElementById("table-content");
 const selectEval = document.getElementById("idPlan");
 const asigName = document.getElementById("asig");
+const btn_act = document.getElementById("btn_act");
+const btn_update = document.getElementById("btn_update");
 const grupoName = document.getElementById("grupo");
 const statusLoading = document.getElementById("loading");
 
@@ -37,14 +39,14 @@ async function main() {
 
   const res = await fetch(urlApi);
   const { data } = await res.json();
-
+  
   let content = "";
   data.forEach((item, index) => {
     content += "<tr>";
     content += `<td>${index + 1}</td>`;
     content += `<td>${item.PATERNO} ${item.MATERNO} ${item.NOMBRE}</td>`;
     content += `<td>${item.MATRICULA}</td>`;
-    content += `<td><input size="3" value="${item.CALIFICACION}" ></input></td>`;
+    content += `<td><input size="3" value="${item.CALIFICACION}" disabled></input></td>`;
     content += "</tr>";
     
   });
@@ -52,6 +54,32 @@ async function main() {
   statusLoading.classList.add("d-none");
   table.innerHTML = content;
 }
+
+//para boton de habilitar de editar
+btn_act.addEventListener("click", () => {
+  btn_act.classList.add("d-none");
+  btn_update.classList.remove("d-none");
+   
+   let inputs = document.getElementsByTagName("input");
+  let selects = document.getElementsByTagName("select");
+  let txtarea = document.getElementsByTagName("textarea");
+
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].disabled = false;
+  }
+
+  for (let i = 0; i < selects.length; i++) {
+    selects[i].disabled = false;
+  }
+
+  for (let i = 0; i < txtarea.length; i++) {
+    txtarea[i].disabled = false;
+  }
+
+   //mandar a inicio cuando se habilite
+   document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 
 selectEval.addEventListener("change", ({ target }) => 
 {  params["idEval"] = target.value;
--- src/utils/dashboard-items.js
+++ src/utils/dashboard-items.js
@@ -21,17 +21,17 @@ const admin_items = [
 
 const alumno_items = [
   {
+    path_img: "./imgs/datos-del-usuario.png",
+    title: "Datos personales",
+    content:
+      "Ingresa y actualiza tu dirección y teléfono; la persona autorizada para recibir información, datos de becas y seguro médico",
+    link: "/perfil",
+  },
+  {
     path_img: "./imgs/documentos.png",
     title: "Documentos del alumno",
     content: "Ingresa y sube los documentos en formato.pdf",
     link: "/doctos",
-  },
-  {
-    path_img: "./imgs/datos-del-usuario.png",
-    title: "Documentos personales",
-    content:
-      "Ingresa y actualiza tu dirección y teléfono; la persona autorizada para recibir información, datos de becas y seguro médico",
-    link: "/perfil",
   },
   {
     path_img: "./imgs/satisfaccion.png",

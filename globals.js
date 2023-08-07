41efd84cd274f85cab72d6a17bce093e2941a437
GermanSima8 <126793096+GermanSima8@users.noreply.github.com>
2023-08-07 11:04:36 -0600

    Add files via upload

---
+++ globals.js
@@ -0,0 +1,22 @@
+// Aquí se pueden de definir variables visibles que se usarán en el proyecto
+
+//cuidado al cambiar las variables locals, ya que afecta todas las funciones que lo utilizan
+
+const globals = (req, res, next) => {
+  // Mensajes de retorno html
+  res.locals.msj_error = req.flash("msj_error");
+  res.locals.msj_good = req.flash("msj_good");
+
+  // Variables globales para mostrar en la web
+  res.locals.isAuthenticated = req.session.isAuthenticated;
+  res.locals.IDAuth = req.session.IDAuth;
+  res.locals.nameAuth = req.session.nameAuth;
+  res.locals.lastNameAuth = req.session.lastNameAuth;
+  res.locals.isAdmin = req.session.isAdmin;
+  res.locals.isAlumno = req.session.isAlumno;
+  res.locals.isProfe = req.session.isProfe;
+  res.locals.isAspirante = req.session.isAspirante;
+  next();
+};
+
+module.exports = globals;
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+const fileUpload = require('express-fileupload');
+require("dotenv").config();
+
+const app = express();
+
+// Definiendo la ruta para acceder en los archivos desde las etiquetas html
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configuracion de plantillas Handlebars
+app.set("views", path.join(__dirname, "src", "views")); // Definiendo las rutas de las vistas
+app.engine(
+  ".hbs",
+  engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"), // Definiendo la vista principal
+    partialsDir: path.join(app.get("views"), "layouts"), // Definiendo las extenciones para la vista principal
+    extname: ".hbs", // Definiendo la extencion para las vistas
+  })
+);
+app.set("view engine", ".hbs");
+
+// Middlewares
+app.use(express.json()); // Admite en el request datos tipo json
+app.use(express.urlencoded({ extended: false })); // Lee los resultados de los formularios en el request
+app.use(fileUpload())
+
+// Configurando las sesiones
+app.use(session({
+  secret: "keyboard cat",
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(flash());
+
+// Variables Globales
+app.use(require('./globals'));
+
+// Ruta de las apis
+app.use('/api', require('./src/routes/apis'));
+
+// Aqui se definen las rutas
+app.use(require("./src/routes/routes"));
+
+// Aqui se levanta el servidor y se define el puerto
+let port = process.env.PORT || 8080;
+app.listen(port, () =>
+  console.log("Servidor corriendo en http://localhost:" + port)
+);
